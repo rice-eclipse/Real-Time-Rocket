df2f9426966b218a7d5b98959bd7ece7f4d44bf8
Andrew Obler <andrewobler@gmail.com>
2018-11-07 18:48:10 -0600

    Added barebones transmission test for certs
    Just transmits incrementing numbers as fast as possible and logs to
    local SD. Receiver prints values to serial but does not log to any
    external source yet. Kept original factory examples in Examples folder.

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -61,42 +61,48 @@ int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 void loop()
 {
-Serial.println("Sending to rf95_server");
+//Serial.println("Sending to rf95_server");
 // Send a message to rf95_server
 
-char radiopacket[20] = "Hello World #      ";
-itoa(packetnum++, radiopacket+13, 10);
-Serial.print("Sending "); Serial.println(radiopacket);
-radiopacket[19] = 0;
+// Send a timestamp (ms) followed by the number
+char timestamp[13] = "Time        ";
+itoa(millis(), timestamp+5, 10);
+timestamp[12] = 0;
+char number[10] = "#        ";
+itoa(packetnum++, number+1, 10);
+number[9] = 0;
+Serial.print("Sending timestamp: "); Serial.println(timestamp);
+Serial.print("Sending number: "); Serial.println(number);
 
-Serial.println("Sending..."); delay(10);
-rf95.send((uint8_t *)radiopacket, 20);
+//Serial.println("Sending..."); delay(10);
+rf95.send((uint8_t *)timestamp, 13);
+rf95.send((uint8_t *)number, 10);
 
-Serial.println("Waiting for packet to complete..."); delay(10);
-rf95.waitPacketSent();
-// Now wait for a reply
-uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-uint8_t len = sizeof(buf);
+//Serial.println("Waiting for packet to complete..."); delay(10);
+//rf95.waitPacketSent();
+//// Now wait for a reply
+//uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+//uint8_t len = sizeof(buf);
 
-Serial.println("Waiting for reply..."); delay(10);
-if (rf95.waitAvailableTimeout(1000))
-{ 
-  // Should be a reply message for us now   
-  if (rf95.recv(buf, &len))
- {
-    Serial.print("Got reply: ");
-    Serial.println((char*)buf);
-    Serial.print("RSSI: ");
-    Serial.println(rf95.lastRssi(), DEC);    
-  }
-  else
-  {
-    Serial.println("Receive failed");
-  }
-}
-else
-{
-  Serial.println("No reply, is there a listener around?");
-}
-delay(1000);
+//Serial.println("Waiting for reply..."); delay(10);
+//if (rf95.waitAvailableTimeout(1000))
+//{ 
+//  // Should be a reply message for us now   
+//  if (rf95.recv(buf, &len))
+// {
+//    Serial.print("Got reply: ");
+//    Serial.println((char*)buf);
+//    Serial.print("RSSI: ");
+//    Serial.println(rf95.lastRssi(), DEC);    
+//  }
+//  else
+//  {
+//    Serial.println("Receive failed");
+//  }
+//}
+//else
+//{
+//  Serial.println("No reply, is there a listener around?");
+//}
+//delay(1000);
 }
