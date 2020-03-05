e3893bb13100a682ea4de0048223f1fb1f90cfb8
claytonwramsey <claytonwramsey@gmail.com>
2020-03-05 13:19:02 -0600

    Working pressure sensor

--- Arduino Prototype/Altimeter/Altimeter/Altimeter.ino
+++ Arduino Prototype/Altimeter/Altimeter/Altimeter.ino
@@ -18,16 +18,14 @@
 #define PROM_C5 0xAA
 #define PROM_C6 0xAC
 
-#define T_REF 20
 
 
-
-int C1;
-int C2; 
-int C3; 
-int C4;
-int C5;
-int C6;
+uint16_t C1;
+uint16_t C2; 
+uint16_t C3; 
+uint16_t C4;
+uint16_t C5;
+uint16_t C6;
 
 
 void setup() {
@@ -44,21 +42,46 @@ void setup() {
   Serial.println("Device reset");
   Serial.println("Reading PROM");
   update_prom();
+
 }
 
 void loop() {
-  int d1 = read_data(CONVERT_D1_4096);//uncorrected pressure data
-  int d2 = read_data(CONVERT_D2_4096);//uncorrected temp data
+  uint32_t d1 = read_data(CONVERT_D1_4096);//uncorrected pressure data
+  uint32_t d2 = read_data(CONVERT_D2_4096);//uncorrected temp data
 
-  Serial.println(d1);
+  int32_t tRef = C5;
+  tRef = tRef << 8;
 
-  float dT = d2 - (C5 * 256);
-  Serial.println(dT);
-  float temp = 2000 + ((dT * 28312) / 8388608.0);
+  int32_t dT = d2 - tRef;
+
+  long long longtemp = ((long long) dT) * ((long long) C6);//need 64 bits because of overflow errors
+  longtemp = longtemp >> 23;
+  longtemp = longtemp + 2000;
+  int32_t inttemp = (int32_t) longtemp;
+  double temp = ((float) inttemp) / 100;
   Serial.print("Temperature is: ");
   Serial.println(temp);
 
-  delay(500);
+  int64_t off = ((int64_t) C2) << 16;//offset at actual temperature
+  
+  off += (C4*dT) >> 7; 
+  
+  //OLD MATH - has overflows out the wazoo
+  int64_t sens = ((int64_t) C1) << 15;
+  sens += (C3 * dT) >> 8;
+
+  long long longpressure = d1 * sens >> 21;
+  longpressure -= off;
+  longpressure = longpressure >> 15;
+
+  int32_t intpressure = (int32_t) longpressure;
+  //
+  double pressure = ((double) intpressure) / 100;
+  
+  Serial.print("Pressure is: ");
+  Serial.println(pressure);
+
+  delay(1000);
 }
 
 
@@ -76,26 +99,23 @@ void update_prom(){
   Serial.println(C6);
 }
 
-int read_prom(int command){
+uint16_t read_prom(int command){
   
   Wire.beginTransmission(ALT_ADDR);
   Wire.write(command);
   Wire.endTransmission();
 
-  
   delay(100);
   
-
-
   Wire.requestFrom(ALT_ADDR, 2);
 
   delay(100);
   // wait for the bytes to become available
   while (Wire.available() < 1);
 
-  unsigned int reading;
+  uint16_t reading;
   reading |= Wire.read();
-  reading << 8;
+  reading = reading << 8;
   reading |= Wire.read();
   return reading;
 }
@@ -118,11 +138,21 @@ uint32_t read_data(int command) {
   // wait for the bytes to become available
   while (Wire.available() < 3);
 
+  //read off 3x 8-bit numbers
   uint32_t reading;
-  reading |= (uint32_t) Wire.read();
-  reading << 8;
-  reading |= (uint32_t) Wire.read();
-  reading << 8;
-  reading |= (uint32_t) Wire.read();
+  reading = (uint32_t) 0;
+  uint32_t a = Wire.read();
+  a = a << 16;
+  
+  uint32_t b = Wire.read();
+  b = b << 8;
+
+  uint32_t c = Wire.read();
+  
+  c = c << 0;
+
+  reading |= a;
+  reading |= b;
+  reading |= c;
   return reading;
 }
