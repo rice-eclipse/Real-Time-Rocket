0b09b0002b5eb684d33d52986340e42905e14425
Hong-Ye Wang <hw62@rice.edu>
2023-02-05 20:09:17 -0600

    added example code for BNO085

--- Neutro Code/imu.py
+++ Neutro Code/imu.py
@@ -1,1 +1,48 @@
 #code to interface with BNO085 imu
+# update dtparam=i2c_arm_baudrate=400000 to the pi confoiguration at /boot/config.txt
+
+# SPDX-FileCopyrightText: 2020 Bryan Siepert, written for Adafruit Industries
+# SPDX-License-Identifier: Unlicense
+import time
+import board
+import busio
+from adafruit_bno08x import {
+    BNO_REPORT_ACCELEROMETER,
+    BNO_REPORT_GYROSCOPE,
+    BNO_REPORT_MAGNETOMETER,
+    BNO_REPORT_ROTATION_VECTOR,
+} 
+from adafruit_bno08x.i2c import BNO08X_I2C
+
+# i2c initialization
+i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
+bno = BNO08X_I2C(i2c)
+
+# enables us to get the accelerometer, gyroscope, magnetomer, and quaternion of our rotation
+bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+bno.enable_feature(BNO_REPORT_GYROSCOPE)
+bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+
+# recording data for the four parameters
+while True:
+    time.sleep(1)
+    print("Acceleration (m/s^2):")
+    accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member? what does this mean
+    print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
+    print("")
+
+    print("Gyro (rad/sec):")
+    gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
+    print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
+    print("")
+
+    print("Magnetometer (microteslas):")
+    mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
+    print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
+    print("")
+
+    print("Rotation Vector Quaternion:")
+    quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
+    print("I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real))
+    print("")
