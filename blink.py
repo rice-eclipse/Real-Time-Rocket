a1ea5d8f24e3908d1d2bf6719e7a543efbd09f3f
Hong-Ye Wang <hw62@rice.edu>
2023-02-20 21:46:50 -0600

    turn off light after blinking

--- blink.py
+++ blink.py
@@ -9,10 +9,14 @@ led_green = digitalio.DigitalInOut(board.D7)
 led_red.direction = digitalio.Direction.OUTPUT
 led_green.direction = digitalio.Direction.OUTPUT
 
-while True:
-    led_red.value = True
-    led_green.value = False
-    time.sleep(0.5)
+try:
+    while True:
+        led_red.value = True
+        led_green.value = False
+        time.sleep(0.75)
+        led_red.value = False
+        led_green.value = True
+        time.sleep(0.75)
+except InterruptedError:
     led_red.value = False
-    led_green.value = True
-    time.sleep(0.5)
+    led_green.value = False
