8bf28188b02ea9f99605444082e88e34792696ff
Hong-Ye Wang <hw62@rice.edu>
2023-03-24 16:47:07 -0500

    blink 10 times

--- blink.py
+++ blink.py
@@ -8,15 +8,20 @@ led_red = digitalio.DigitalInOut(board.D8)
 led_green = digitalio.DigitalInOut(board.D7)
 led_red.direction = digitalio.Direction.OUTPUT
 led_green.direction = digitalio.Direction.OUTPUT
+count = 0
 
 try:
     while True:
+        
         led_red.value = True
         led_green.value = False
         time.sleep(0.75)
         led_red.value = False
         led_green.value = True
         time.sleep(0.75)
+        count += 1
+        if count == 10:
+            break
 except KeyboardInterrupt:
     led_red.value = False
     led_green.value = False
