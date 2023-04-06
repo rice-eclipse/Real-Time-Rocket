f9303f3496b4e11e08aaf15b560dc68a9f639179
Hong-Ye Wang <hw62@rice.edu>
2023-04-05 20:08:44 -0500

    remove non pcb related files, stored in other branches

--- launchTest/examples/example_tx.py
+++
@@ -1,9 +0,0 @@
-from radio_launch_test import Radio
-import time
-
-trx = Radio("example_config.yaml", None)
-
-while True:
-    example_data = {'latitude': 29.72, 'longitude': -95.41, 'fix_quality': 2}
-    trx.send(example_data)
-    # time.sleep(1)
