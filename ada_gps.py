f477e8781932343fc6bd39231dc468582edf28b3
Hong-Ye Wang <hw62@rice.edu>
2023-05-22 14:03:05 -0500

    remove precision format

--- ada_gps.py
+++ ada_gps.py
@@ -11,30 +11,15 @@ import adafruit_gps
 
 # Create a serial connection for the GPS connection using default speed and
 # a slightly higher timeout (GPS modules typically update once a second).
-# These are the defaults you should use for the GPS FeatherWing.
-# For other boards set RX = GPS module TX, and TX = GPS module RX pins.
-uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=10)
-
-# for a computer, use the pyserial library for uart access
-# import serial
-# uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=10)
-
-# If using I2C, we'll create an I2C interface to talk to using default pins
-# i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+# For a computer, use the pyserial library for uart access
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=30)
 
 # Create a GPS module instance.
-gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
-# gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)  # Use I2C interface
-
-# Initialize the GPS module by changing what data it sends and at what rate.
-# These are NMEA extensions for PMTK_314_SET_NMEA_OUTPUT and
-# PMTK_220_SET_NMEA_UPDATERATE but you can send anything from here to adjust
-# the GPS module behavior:
-#   https://cdn-shop.adafruit.com/datasheets/PMTK_A11.pdf
+gps = adafruit_gps.GPS(uart, debug=False)
 
 # Turn on the basic GGA and RMC info (what you typically want)
 gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+
 # Turn on just minimum info (RMC only, location):
 # gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
 # Turn off everything:
@@ -43,16 +28,11 @@ gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
 # gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
 
 # Set update rate to once a second (1hz) which is what you typically want.
-gps.send_command(b"PMTK220,1000")
-# Or decrease to once every two seconds by doubling the millisecond value.
-# Be sure to also increase your UART timeout above!
-# gps.send_command(b'PMTK220,2000')
-# You can also speed up the rate, but don't go too fast or else you can lose
-# data during parsing.  This would be twice a second (2hz, 500ms delay):
-# gps.send_command(b'PMTK220,500')
+gps.send_command(b"PMTK220,200")
 
 # Main loop runs forever printing the location, etc. every second.
 last_print = time.monotonic()
+
 while True:
     # Make sure to call gps.update() every loop iteration and at least twice
     # as fast as data comes from the GPS unit (usually every second).
@@ -82,28 +62,28 @@ while True:
         )
         print("Latitude: {0:.6f} degrees".format(gps.latitude))
         print("Longitude: {0:.6f} degrees".format(gps.longitude))
-        print(
-            "Precise Latitude: {:2.}{:2.4f} degrees".format(
-                gps.latitude_degrees, gps.latitude_minutes
-            )
-        )
-        print(
-            "Precise Longitude: {:2.}{:2.4f} degrees".format(
-                gps.longitude_degrees, gps.longitude_minutes
-            )
-        )
+        # print(
+        #     "Precise Latitude: {:2.}{:2.4f} degrees".format(
+        #         gps.latitude_degrees, gps.latitude_minutes
+        #     )
+        # )
+        # print(
+        #     "Precise Longitude: {:2.}{:2.4f} degrees".format(
+        #         gps.longitude_degrees, gps.longitude_minutes
+        #     )
+        # )
         print("Fix quality: {}".format(gps.fix_quality))
         # Some attributes beyond latitude, longitude and timestamp are optional
         # and might not be present.  Check if they're None before trying to use!
-        if gps.satellites is not None:
-            print("# satellites: {}".format(gps.satellites))
-        if gps.altitude_m is not None:
-            print("Altitude: {} meters".format(gps.altitude_m))
-        if gps.speed_knots is not None:
-            print("Speed: {} knots".format(gps.speed_knots))
-        if gps.track_angle_deg is not None:
-            print("Track angle: {} degrees".format(gps.track_angle_deg))
-        if gps.horizontal_dilution is not None:
-            print("Horizontal dilution: {}".format(gps.horizontal_dilution))
-        if gps.height_geoid is not None:
-            print("Height geoid: {} meters".format(gps.height_geoid))
+        # if gps.satellites is not None:
+        #     print("# satellites: {}".format(gps.satellites))
+        # if gps.altitude_m is not None:
+        #     print("Altitude: {} meters".format(gps.altitude_m))
+        # if gps.speed_knots is not None:
+        #     print("Speed: {} knots".format(gps.speed_knots))
+        # if gps.track_angle_deg is not None:
+        #     print("Track angle: {} degrees".format(gps.track_angle_deg))
+        # if gps.horizontal_dilution is not None:
+        #     print("Horizontal dilution: {}".format(gps.horizontal_dilution))
+        # if gps.height_geoid is not None:
+        #     print("Height geoid: {} meters".format(gps.height_geoid))
