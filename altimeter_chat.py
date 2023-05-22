f1cc9cb6ccafc10dcdfcaaf2d9faf9043f524bc1
Hong-Ye Wang <hw62@rice.edu>
2023-05-22 14:06:46 -0500

    comment trx on altimeter

--- altimeter_chat.py
+++ altimeter_chat.py
@@ -4,7 +4,7 @@ from radio_launch_test import Radio
 
 P0 = 1010.837  # Approximate sea level pressure in mbar at Galveston
 
-trx = Radio("/home/pi/Neutro/neutro_config_32523.yaml", None)
+#trx = Radio("/home/pi/Neutro/neutro_config_32523.yaml", None)
 
 altimeter = MS5803(0)
 
@@ -13,6 +13,6 @@ while True:
     pressure, temp = altimeter.read()
     h = ((P0 / pressure) ** (1 / 5.257) - 1) * (temp + 273.15) / 0.0065
     data = {'pressure': pressure, 'temperature': temp, 'altitude': h}
-    trx.send(data)
+    #trx.send(data)
     print(data)
     time.sleep(1.0)
