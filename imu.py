490f216a88183a7b7d8da37d2ce0b4543e62789a
Hong-Ye Wang <hw62@rice.edu>
2023-05-22 14:29:28 -0500

    note on imu not working

--- imu.py
+++ imu.py
@@ -1,25 +1,27 @@
-# SPDX-FileCopyrightText: 2020 Bryan Siepert, written for Adafruit Industries
-#
-# SPDX-License-Identifier: Unlicense
+# As of 5/22/23, there is soldering/connection issues so the imu is only initialized some of the times
+# and it is unable to enable the sensors
 import time
 import board
 import busio
-
 from adafruit_bno08x import (
     BNO_REPORT_ACCELEROMETER,
     BNO_REPORT_GYROSCOPE,
     BNO_REPORT_MAGNETOMETER,
     BNO_REPORT_ROTATION_VECTOR,
 )
-
 from adafruit_bno08x.i2c import BNO08X_I2C
 
+# initialize bno object
 i2c = busio.I2C(board.D3, board.D2, frequency=400000)
 bno = BNO08X_I2C(i2c)
 print("initialized!")
 
 # Print BNO085 calibration status.
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+bno.enable_feature(BNO_REPORT_GYROSCOPE)
+bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+
 
 while True:
     accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
